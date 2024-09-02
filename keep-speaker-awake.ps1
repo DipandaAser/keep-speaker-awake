37a9c9e3851c79377d10e5409ad2bf01481d6b22
Aser <aserdipanda@gmail.com>
2024-09-02 11:01:12 +0100

    change wake up sound

--- keep-speaker-awake.ps1
+++ keep-speaker-awake.ps1
@@ -1,12 +1,12 @@
-$delayInMinute = 5
+$delayInMinute = 15
 $running = $true
 
 while ($running) {
     # Your commands to be executed repeatedly go here
     Write-Host -ForegroundColor Green ($(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')) "Playing sound again"
     $PlayWav = New-Object System.Media.SoundPlayer
-    $PlayWav.SoundLocation = '.\silent.wav'
-    $PlayWav.PlaySync()
+    $PlayWav.SoundLocation = '.\notif.wav'
+    $PlayWav.Play()
 
     # Sleep for the specified delay
     Start-Sleep -Seconds ($delayInMinute * 60)
